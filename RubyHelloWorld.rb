5bb2acb6b3b5b8bf69420c0531bb9b4f5276d001
soplist <sopcase@yahoo.com>
2016-05-03 13:30:29 +0800

    ruby

---
+++ RubyHelloWorld.rb
@@ -0,0 +1,20 @@
+#Ruby HelloWorld  
+      
+#v1.0  
+def say_hello_1(name)  
+  return "Hello , "+ name    
+end  
+      
+#v2.0  
+def say_hello_2 name  
+  return "Hello , " + name  
+end  
+      
+#v3.0  
+def say_hello_3 name  
+  return "Hello , #{name}"  
+end  
+      
+puts say_hello_1 "wangkang_1"  
+puts say_hello_2 "wangkang_2"  
+puts say_hello_3 "wangkang_3"  
